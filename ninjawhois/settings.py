a7f531f0b10658af7824ac03cb4f8302dbb8088d
Robin <simardrobin@outlook.com>
2020-07-12 11:49:11 -0700

    modified static url

--- ninjawhois/settings.py
+++ ninjawhois/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
