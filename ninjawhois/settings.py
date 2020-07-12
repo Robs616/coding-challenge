23a34fda66ae57480387bd88c39e33be5120ba60
Robin <simardrobin@outlook.com>
2020-07-12 13:05:46 -0700

    modified static

--- ninjawhois/settings.py
+++ ninjawhois/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,12 +125,8 @@ USE_TZ = True
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static")]
 
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
